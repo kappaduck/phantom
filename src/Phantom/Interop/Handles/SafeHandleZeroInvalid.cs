a3ca91acf0404b639e8611b0d44513ebdd947b86
beauchama <beauchama@kappaduck.com>
2025-03-07 18:18:13 -0500

    Added custom marshallers to manage array/strings

--- src/Phantom/Interop/Handles/SafeHandleZeroInvalid.cs
+++ src/Phantom/Interop/Handles/SafeHandleZeroInvalid.cs
@@ -9,10 +9,7 @@ namespace Phantom.Interop.Handles;
 /// Provides a base class for safe handle implementations in which the value of <see cref="nint.Zero"/> indicates an invalid handle.
 /// </summary>
 /// <param name="ownsHandle"><see langword="true"/> owns the handle during the finalization phase; otherwise, <see langword="false"/>.</param>
-public abstract class SafeHandleZeroInvalid(bool ownsHandle) : SafeHandle(nint.Zero, ownsHandle)
+internal abstract class SafeHandleZeroInvalid(bool ownsHandle) : SafeHandle(nint.Zero, ownsHandle)
 {
-    /// <summary>
-    /// Gets a value indicating whether the handle value is invalid.
-    /// </summary>
     public override bool IsInvalid => handle == nint.Zero;
 }
