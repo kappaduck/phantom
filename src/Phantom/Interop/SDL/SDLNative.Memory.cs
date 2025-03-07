881caca69fe9fc31d9ca7a0ae505395068fd03fb
beauchama <beauchama@kappaduck.com>
2025-03-07 18:41:48 -0500

    Added custom marshallers, specialized safe handle and targets

---
+++ src/Phantom/Interop/SDL/SDLNative.Memory.cs
@@ -0,0 +1,22 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Phantom.Interop.SDL;
+
+internal static partial class SDLNative
+{
+    internal static unsafe void Free<T>(T* memory) where T : unmanaged
+        => Free((nint)memory);
+
+    internal static unsafe void Free<T>(T** memory) where T : unmanaged
+        => Free((nint)memory);
+
+    internal static void Free(nint memory) => SDL_free(memory);
+
+    [LibraryImport(Name)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    private static partial void SDL_free(nint memory);
+}
