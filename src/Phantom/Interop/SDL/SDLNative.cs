a416693dc7fa2dafcb9065a66157e34b7f079298
beauchama <beauchama@kappaduck.com>
2025-03-07 18:17:30 -0500

    Added native methods to free memory

---
+++ src/Phantom/Interop/SDL/SDLNative.cs
@@ -0,0 +1,9 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Phantom.Interop.SDL;
+
+internal static partial class SDLNative
+{
+    internal const string Name = "SDL3";
+}
