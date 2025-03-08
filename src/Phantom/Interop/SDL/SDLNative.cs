86528f0e18e6990fab54878193fd22975692c470
beauchama <beauchama@kappaduck.com>
2025-03-07 21:04:51 -0500

    Added PhantomEngine

--- src/Phantom/Interop/SDL/SDLNative.cs
+++ src/Phantom/Interop/SDL/SDLNative.cs
@@ -1,6 +1,7 @@
 // Copyright (c) KappaDuck. All rights reserved.
 // The source code is licensed under MIT License.
 
+using Phantom.Core;
 using Phantom.Interop.SDL.Marshallers;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -20,4 +21,25 @@ internal static partial class SDLNative
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     [return: MarshalUsing(typeof(SDLOwnedStringMarshaller))]
     internal static partial string SDL_GetError();
+
+    [LibraryImport(Name)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial ulong SDL_GetTicks();
+
+    [LibraryImport(Name)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int SDL_GetVersion();
+
+    [LibraryImport(Name)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool SDL_InitSubSystem(SubSystem subSystem);
+
+    [LibraryImport(Name)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial void SDL_QuitSubSystem(SubSystem subSystem);
+
+    [LibraryImport(Name)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial void SDL_Quit();
 }
