6a235caa5f23eae97307ed7832c4ffd277080712
beauchama <beauchama@kappaduck.com>
2025-03-07 21:03:42 -0500

    Added exceptions

--- src/Phantom/Interop/SDL/SDLNative.cs
+++ src/Phantom/Interop/SDL/SDLNative.cs
@@ -1,9 +1,23 @@
 // Copyright (c) KappaDuck. All rights reserved.
 // The source code is licensed under MIT License.
 
+using Phantom.Interop.SDL.Marshallers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
 namespace Phantom.Interop.SDL;
 
 internal static partial class SDLNative
 {
     internal const string Name = "SDL3";
+
+    [LibraryImport(Name)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial void SDL_ClearError();
+
+    [LibraryImport(Name)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalUsing(typeof(SDLOwnedStringMarshaller))]
+    internal static partial string SDL_GetError();
 }
