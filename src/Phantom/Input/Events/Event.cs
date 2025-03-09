8534d7b7bdbe0703a08ffc940c8c607930096574
beauchama <beauchama@kappaduck.com>
2025-03-09 14:34:58 -0400

    Added Keyboard with events

--- src/Phantom/Input/Events/Event.cs
+++ src/Phantom/Input/Events/Event.cs
@@ -18,6 +18,18 @@ public struct Event
     public readonly EventType Type;
 
     /// <summary>
+    /// The keyboard device event data.
+    /// </summary>
+    [FieldOffset(0)]
+    public readonly KeyboardDeviceEvent KeyboardDevice;
+
+    /// <summary>
+    /// The keyboard event data.
+    /// </summary>
+    [FieldOffset(0)]
+    public readonly KeyboardEvent Keyboard;
+
+    /// <summary>
     /// The mouse button event data.
     /// </summary>
     [FieldOffset(0)]
--- src/Phantom/Interop/SDL/SDLNative.Input.cs
+++ src/Phantom/Interop/SDL/SDLNative.Input.cs
@@ -17,8 +17,36 @@ internal static partial class SDLNative
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial Keyboard.Keycode SDL_GetKeyFromScancode(Keyboard.Scancode code, Keyboard.Modifier modifier, [MarshalAs(UnmanagedType.I1)] bool keyEvents);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalUsing(typeof(SDLOwnedStringMarshaller))]
+    internal static partial string SDL_GetKeyName(Keyboard.Keycode code);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalUsing(typeof(SDLOwnedStringMarshaller))]
+    internal static partial string SDL_GetKeyboardNameForID(uint keyboard);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalUsing(typeof(CallerOwnedArrayMarshaller<,>), CountElementName = "length")]
+    internal static partial Span<uint> SDL_GetKeyboards(out int length);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalUsing(typeof(SDLOwnedArrayMarshaller<,>), CountElementName = "length")]
+    internal static partial Span<byte> SDL_GetKeyboardState(out int length);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     [return: MarshalUsing(typeof(CallerOwnedArrayMarshaller<,>), CountElementName = "length")]
     internal static partial Span<uint> SDL_GetMice(out int length);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial Keyboard.Modifier SDL_GetModState();
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
@@ -35,8 +63,48 @@ internal static partial class SDLNative
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial Keyboard.Scancode SDL_GetScancodeFromKey(Keyboard.Keycode code, out Keyboard.Modifier modifier);
+
+    [LibraryImport(LibraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial Keyboard.Scancode SDL_GetScancodeFromName(string name);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalUsing(typeof(SDLOwnedStringMarshaller))]
+    internal static partial string SDL_GetScancodeName(Keyboard.Scancode code);
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool SDL_HasKeyboard();
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool SDL_HasScreenKeyboardSupport();
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     [return: MarshalAs(UnmanagedType.I1)]
     internal static partial bool SDL_HasMouse();
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial void SDL_ResetKeyboard();
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial void SDL_SetModState(Keyboard.Modifier modifier);
+
+    /// <summary>
+    /// The string is not copied, so it must be valid for the lifetime of the application.
+    /// The source generator will free the string so we need to pass a <see cref="Span{T}"/> of byte instead to keep in memory.
+    /// </summary>
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalAs(UnmanagedType.I1)]
+    internal static partial bool SDL_SetScancodeName(Keyboard.Scancode code, Span<byte> name);
 
     [LibraryImport(LibraryName)]
     [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
