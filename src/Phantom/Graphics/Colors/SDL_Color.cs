c974a91d4468ff510ca58c5ce3a5e458718b74f8
beauchama <beauchama@kappaduck.com>
2025-03-11 22:07:59 -0400

    Added Colors

---
+++ src/Phantom/Graphics/Colors/SDL_Color.cs
@@ -0,0 +1,29 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Phantom.Graphics.Colors;
+
+[StructLayout(LayoutKind.Sequential)]
+internal readonly struct SDL_Color
+{
+    internal SDL_Color(byte r, byte g, byte b, byte a)
+    {
+        _r = r;
+        _g = g;
+        _b = b;
+        _a = a;
+    }
+
+    private readonly byte _r;
+    private readonly byte _g;
+    private readonly byte _b;
+    private readonly byte _a;
+
+    /// <summary>
+    /// Gets the color.
+    /// </summary>
+    internal Color Color => Color.FromArgb(_a, _r, _g, _b);
+}
