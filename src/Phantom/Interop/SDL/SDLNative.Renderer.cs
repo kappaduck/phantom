d7a1db67a16934239c662e82340a9033c3e21300
beauchama <beauchama@kappaduck.com>
2025-03-11 23:54:27 -0400

    Added Renderer

---
+++ src/Phantom/Interop/SDL/SDLNative.Renderer.cs
@@ -0,0 +1,21 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Phantom.Interop.SDL.Marshallers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Phantom.Interop.SDL;
+
+internal static partial class SDLNative
+{
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    internal static partial int SDL_GetNumRenderDrivers();
+
+    [LibraryImport(LibraryName)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [return: MarshalUsing(typeof(SDLOwnedStringMarshaller))]
+    internal static partial string SDL_GetRenderDriver(int index);
+}
